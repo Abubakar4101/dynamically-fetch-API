4d6c5eb5b7f241ec3fe991893136d15a6e68eb96
M.Abubakar Siddique <abubakarsiddique4101@gmail.com>
2023-08-04 16:49:05 +0500

    app added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Exo } from 'next/font/google'
+
+const exo = Exo({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <main className={`${exo.className}`}>
+        <Component {...pageProps} />
+      </main>
+  )
 }
